b1e217c83f90ef40deedcfd549421469ba4d6531
Christianrslv <christianrslv22@gmail.com>
2022-04-12 19:46:54 -0300

    Fix: Balance IP set enviroments

--- frontend/src/app/shared/services/weighing-scale.service.ts
+++ frontend/src/app/shared/services/weighing-scale.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class WeighingScaleService {
 
+    baseUrl = environment.balanceUrl;
+
     constructor(private http: HttpClient) {
     }
 
     getWeightWeighingScale(): Observable<number> {
-        return this.http.get<number>('http://' + localStorage.getItem('WeighingScaleIP') + ':80');
+        // return this.http.get<number>('http://' + localStorage.getItem('WeighingScaleIP') + ':80');
+        return this.http.get<number>(this.baseUrl);
     }
 }
